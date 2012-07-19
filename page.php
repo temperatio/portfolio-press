96ebd431095185192afbff564cde055b3dba0541
César Gómez Antonio <cesar@temperatio.com>
2012-07-19 18:15:56 +0200

    Add hideable title Custom Attribute for pages

--- page.php
+++ page.php
@@ -12,10 +12,13 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<header class="entry-header">
-					<h1 class="entry-title"><?php the_title(); ?></h1>
-				</header><!-- .entry-header -->
-
+				
+				<? if(!get_post_meta($post->ID, 'hidetitle', true)) :?>
+					<header class="entry-header">
+						<h1 class="entry-title"><?php the_title(); ?></h1>
+					</header><!-- .entry-header -->
+				<?php endif; ?>
+				
 				<div class="entry-content">
 					<?php the_content(); ?>
 					<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'portfoliopress' ), 'after' => '</div>' ) ); ?>
